c1da6cdfea5d00fcc42dfef457b793d66b7758a9
강예슬 <gang-yeseul@gang-yeseul-ui-MacBookAir.local>
2024-01-18 16:47:10 +0900

    Refactor

--- 09) 38, 39, 40장/InputTimer_yesul/index.js
+++ 09) 38, 39, 40장/InputTimer_yesul/index.js
@@ -3,10 +3,10 @@ const $button = document.querySelector('button');
 const buttonClickHandler = function() {
   var e = document.getElementById('time').value;
   if (e > 0 && Number.isInteger(+e) === true) {
-    for (let i = e; i > 0; i--) {
+    for (let i = e - 1; i >= 0; i--) {
       setTimeout(() => {
-        document.getElementById('result').innerText = i - 1;
-      }, (e - (i - 1)) * 1000);
+        document.getElementById('result').innerText = i;
+      }, (e - i) * 1000);
     }
   }
   if (e == 0) {
